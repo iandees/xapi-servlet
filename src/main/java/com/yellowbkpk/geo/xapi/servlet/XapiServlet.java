87c70664f24fee1d12ed564ba220e080984e17e2
Ian Dees <ian.dees@gmail.com>
2010-12-19 22:32:41 -0600

    Add a little bit of meat to the servlet.

--- src/main/java/com/yellowbkpk/geo/xapi/servlet/XapiServlet.java
+++ src/main/java/com/yellowbkpk/geo/xapi/servlet/XapiServlet.java
@@ -10,6 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 public class XapiServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// Parse URL
+		String requestedUrl = request.getRequestURL().toString();
+		String query = requestedUrl.substring(requestedUrl.lastIndexOf('/'));
+		
+		// Query DB
+		
+		// Serialize output
 		response.getWriter().println("Hello World");
 	}
 }
