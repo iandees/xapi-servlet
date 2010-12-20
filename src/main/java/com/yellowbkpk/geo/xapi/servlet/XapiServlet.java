5476f69bb68ec720d37554c4305e0cfe10f5585d
Ian Dees <ian.dees@gmail.com>
2010-12-20 15:18:39 -0600

    Link back to the xapi-antlr code and get the servlet talking ANTLR.

--- src/main/java/com/yellowbkpk/geo/xapi/servlet/XapiServlet.java
+++ src/main/java/com/yellowbkpk/geo/xapi/servlet/XapiServlet.java
@@ -7,12 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+
+import com.yellowbkpk.geo.xapi.antlr.XAPILexer;
+import com.yellowbkpk.geo.xapi.antlr.XAPIParser;
+
 public class XapiServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Parse URL
-		String requestedUrl = request.getRequestURL().toString();
-		String query = requestedUrl.substring(requestedUrl.lastIndexOf('/'));
+		try {
+			String requestedUrl = request.getRequestURL().toString();
+			String query = requestedUrl.substring(requestedUrl.lastIndexOf('/') + 1);
+			CharStream stream = new ANTLRStringStream(query);
+			XAPILexer lexer = new XAPILexer(stream);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			XAPIParser parser = new XAPIParser(tokens);
+			parser.xapi().getTree();
+		} catch (RecognitionException e) {
+			response.sendError(500, "Could not parse query: " + e.getMessage());
+		}
 		
 		// Query DB
 		
