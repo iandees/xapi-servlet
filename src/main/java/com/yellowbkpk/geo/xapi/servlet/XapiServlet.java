60114157cb110109d1376c861a662dc590f600d8
Ian Dees <ian.dees@gmail.com>
2010-12-20 22:14:26 -0600

    URL decode the query and show some useful text in the response.

--- src/main/java/com/yellowbkpk/geo/xapi/servlet/XapiServlet.java
+++ src/main/java/com/yellowbkpk/geo/xapi/servlet/XapiServlet.java
@@ -1,6 +1,8 @@
 package com.yellowbkpk.geo.xapi.servlet;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.antlr.runtime.RecognitionException;
+import org.openstreetmap.osmosis.pgsnapshot.v0_6.impl.Selector;
 
 import com.yellowbkpk.geo.xapi.XAPIQueryInfo;
 
@@ -15,10 +18,11 @@ public class XapiServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Parse URL
-		XAPIQueryInfo info;
+		XAPIQueryInfo info = null;
 		try {
 			String requestedUrl = request.getRequestURL().toString();
 			String query = requestedUrl.substring(requestedUrl.lastIndexOf('/') + 1);
+			query = URLDecoder.decode(query, "UTF-8");
 			info = XAPIQueryInfo.fromString(query);
 		} catch (RecognitionException e) {
 			response.sendError(500, "Could not parse query: " + e.getMessage());
@@ -27,6 +31,9 @@ public class XapiServlet extends HttpServlet {
 		// Query DB
 		
 		// Serialize output
-		response.getWriter().println("Hello World");
+		response.getWriter().println(info.getKind() + " request:");
+		for (Selector select : info.getSelectors()) {
+			response.getWriter().println("  " + select.getWhereString() + ", " + Arrays.toString(select.getWhereParam()));
+		}
 	}
 }
